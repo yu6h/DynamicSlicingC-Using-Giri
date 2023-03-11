b56da6a769bb5cf5cf79348543168166e88e02e7
YuHong <jack1996yuc@gmail.com>
2023-03-10 23:26:33 -0800

    移除用不到的註解

--- DynamicSlice/src/Main.java
+++ DynamicSlice/src/Main.java
@@ -29,15 +29,7 @@ public class Main {
 		for(Integer lineNumber:lineNumbersOfResult) {
 			System.out.println(lineNumber);
 		}
-//		DynamicSlicingUsedGiri dynamicSlicing = new DynamicSlicingUsedGiri();
-//		
-//
-//		dynamicSlicing.setQuestiotnID("1");
-//		dynamicSlicing.setStudentID("110598067");
-//		dynamicSlicing.setcFileName("DD.c");
-//		dynamicSlicing.setInputData("2 1");
-//		dynamicSlicing.setLineNumberOfTargetStatement(18);		
-//		dynamicSlicing.execute();
+
 	}
 
 }
